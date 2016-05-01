877da2c3bf054a85433e5d90cb971e53bb3b35ee
Joao Norim <norim_13@hotmail.com>
2016-05-01 22:54:46 +0100

    panorama com pannellum e action nos hotspots.

--- cdm-app/www/js/panorama.js
+++ cdm-app/www/js/panorama.js
@@ -19,26 +19,31 @@ function panorama_init(){
         maxHfov: 40,
         minHfov: 30,
         "autoLoad": true,
+        hotSpotDebug: true,
         "hotSpots": [
             {
                 "pitch": 14.1,
                 "yaw": 1.5,
                 "type": "info",
-                "text": "Baltimore Museum of Art",
-                "URL": "https://artbma.org/"
+                "text": '<a id="hotspot1" title="Click to do something" href="#" onclick="openPopup();return false;">link text</a>'/*,
+                "URL": "https://artbma.org/"*/
             },
             {
                 "pitch": -9.4,
                 "yaw": 222.6,
                 "type": "info",
-                "text": "Art Museum Drive"
+                "text": '<a id="hotspot2" title="Click to do something" href="#" onclick="openPopup();return false;">link text</a>'
             },
             {
                 "pitch": -0.9,
                 "yaw": 144.4,
                 "type": "info",
-                "text": "North Charles Street"
+                "text": '<a id="hotspot3" title="Click to do something" href="#" onclick="openPopup();return false;">link text</a>'
             }
         ]
     });
+}
+
+function openPopup(element){
+    window.alert('teste');
 }
