ddf764312755417812b0785e544d4a387772f93d
Joao Norim <norim_13@hotmail.com>
2016-05-01 01:44:33 +0100

    garbage commit. problem pulling.

---
+++ app/www/js/models_3d_info.js
@@ -0,0 +1,36 @@
+/**
+ * Created by João on 30/04/2016.
+ */
+var models = [];
+
+models['casa'] =
+{
+    path : 'obj/casa_da_musica_salas_separadas/casa_da_musica.obj',
+    texture_path: 'obj/casadamusica/texturas/casa_da_musica_uvmap.png',
+    name : 'casa',
+    title : 'Casa'
+};
+
+models['sala_suggia'] =
+{
+    path: 'obj/casa_da_musica_salas_separadas/sala_suggia.obj',
+    texture_path: 'obj/casadamusica/texturas/sala_suggia_uvmap.png',
+    name : 'sala_suggia',
+    title : 'Sala Suggia'
+};
+models['sala_2'] =
+{
+    path : 'obj/casa_da_musica_salas_separadas/sala_concertos2.obj',
+    texture_path: 'obj/casadamusica/texturas/sala_concertos_2_uvmap.png',
+    name : 'sala_2',
+    title : 'Sala 2'
+};
+models['bar'] =
+{
+    path : 'obj/casa_da_musica_salas_separadas/bar.obj',
+    texture_path: 'obj/casadamusica/texturas/Bar_uvmap.png',
+    name : 'bar',
+    title : 'Bar'
+};
+
+
