0a59250eb9e4d354b17a596857062e82be22791f
Joao Norim <norim_13@hotmail.com>
2016-05-05 15:42:37 +0100

    modelos novos.

--- cdm-app/www/js/models_3d_info.js
+++ cdm-app/www/js/models_3d_info.js
@@ -7,8 +7,8 @@ models['casa'] =
 {
     //path : 'obj/casa_da_musica_salas_separadas/casa_da_musica.obj',
     //path: 'obj/experimentar_2/casa_da_musica_experimentar_2.obj',
-    path: 'obj/experimentar_3/casa_da_musica_experimentar_3.obj',
-    texture_path: 'obj/casadamusica/texturas/casa_da_musica_uvmap.png',
+    path: 'obj/casa_da_musica_salas_separadas/casa_da_musica.obj',
+    texture_path: 'obj/casa_da_musica_salas_separadas/texturas/casa_da_musica_uvmap.png',
     name : 'casa',
     title : 'Casa',
     animation : null
@@ -17,15 +17,15 @@ models['casa'] =
 models['sala_suggia'] =
 {
     path: 'obj/casa_da_musica_salas_separadas/sala_suggia.obj',
-    texture_path: 'obj/casadamusica/texturas/sala_suggia_uvmap.png',
+    texture_path: 'obj/casa_da_musica_salas_separadas/texturas/sala_suggia_uvmap.png',
     name : 'sala_suggia',
     title : 'Sala Suggia',
     animation : [20, 0, 0]
 };
 models['sala_2'] =
 {
-    path : 'obj/casa_da_musica_salas_separadas/sala_concertos2.obj',
-    texture_path: 'obj/casadamusica/texturas/sala_concertos_2_uvmap.png',
+    path : 'obj/casa_da_musica_salas_separadas/sala_concertos_2.obj',
+    texture_path: 'obj/casa_da_musica_salas_separadas/texturas/sala_concertos_2_uvmap.png',
     name : 'sala_2',
     title : 'Sala 2',
     animation : [20, 0, 0]
@@ -33,7 +33,7 @@ models['sala_2'] =
 models['bar'] =
 {
     path : 'obj/casa_da_musica_salas_separadas/bar.obj',
-    texture_path: 'obj/casadamusica/texturas/Bar_uvmap.png',
+    texture_path: 'obj/casa_da_musica_salas_separadas/texturas/Bar_uvmap.png',
     name : 'bar',
     title : 'Bar',
     animation : [20, 0, 0]
