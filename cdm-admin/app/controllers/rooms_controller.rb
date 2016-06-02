e3f0c74535bcd0cb00dc761bd27591d0bd33fca5
claudiomonteiro <supahmomo@gmail.com>
2016-06-02 18:38:30 +0100

    voltei a por os cor na api

--- cdm-admin/app/controllers/rooms_controller.rb
+++ cdm-admin/app/controllers/rooms_controller.rb
@@ -1,6 +1,11 @@
 class RoomsController < ApplicationController
+  before_filter :cors_preflight_check, only: [:get_everything]
+  after_filter :cors_set_access_control_headers, only: [:get_everything]
+
   before_action :set_room, only: [:show, :edit, :update, :destroy]
   attr_accessor :room_translations
+
+
   def show
   end
 
@@ -134,4 +139,22 @@ class RoomsController < ApplicationController
     params.require(:room).permit(:code, :model_path, :model_detail_path, :texture_path, :texture_detail_path, :animation, :beacon_uuid, :beacon_major, :beacon_minor, :next_room, room_translations_attributes: [:name, :description, :language_id])
   end
 
+  #cors headers
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+
+  def cors_preflight_check
+    if request.method == 'OPTIONS'
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
+      headers['Access-Control-Max-Age'] = '1728000'
+
+      render :text => '', :content_type => 'text/plain'
+    end
+  end
 end
