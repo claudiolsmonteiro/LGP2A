08789c4126eb56ed4cd58bbb494f6a634e947aeb
Ricardo Figueiredo <ricardojmf>
2016-06-03 01:35:17 +0100

    Points Creation, Points Destroy, Points Index } All Done

--- cdm-admin/app/controllers/points_controller.rb
+++ cdm-admin/app/controllers/points_controller.rb
@@ -1,13 +1,40 @@
 class PointsController < ApplicationController
+
+  def index
+    @points = Point.all
+  end
+
   def show
+    @point = Point.find(params[:id])
   end
 
   def new
+    @point = Point.new
+  end
+
+  def create
+    @point = Point.new(points_params)
+    if @point.save
+      redirect_to @point
+    else
+      render 'f/newpoint'
+    end
   end
 
   def edit
   end
 
-  def index
+  def update
+  end
+
+  def destroy
+    Point.find(params[:id]).destroy
+    redirect_to points_url
+  end
+
+  private
+
+  def points_params
+    params.require(:point).permit(:x, :y, :photo_id)
   end
 end
