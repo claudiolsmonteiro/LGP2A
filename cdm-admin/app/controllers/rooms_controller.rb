1641b1393887fe7bae905f44fe0a0d14e572ec02
claudiomonteiro <supahmomo@gmail.com>
2016-06-03 16:09:00 +0100

    Novo Seeds.rb + mudanças criação de salas

--- cdm-admin/app/controllers/rooms_controller.rb
+++ cdm-admin/app/controllers/rooms_controller.rb
@@ -19,8 +19,6 @@ class RoomsController < ApplicationController
 
   def create
     @room = Room.new(room_params)
-    @room.model_detail_path= @room.model_path
-    @room.texture_detail_path= @room.texture_path
     respond_to do |format|
       if @room.save
 
