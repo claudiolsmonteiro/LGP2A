0c8989667a62eb292b785dc14519d75c52e88eb4
Joao Norim <norim_13@hotmail.com>
2016-06-14 15:23:32 +0100

    corrigido bug na descrição dos pontos.

--- cdm-app/www/js/panorama.js
+++ cdm-app/www/js/panorama.js
@@ -120,7 +120,7 @@ controllerModule.controller("panoramaController", function($scope, $stateParams,
     return "<div class=\"hotspot-box\">"+
         "<p>" + hotspot.translations[$scope.language].title + "</p>" +
         "<a id=\"hotspot_" + hotspot.id + "\" href=\"#\" "+
-        " onclick=\"openPopup(\'" + hotspot.description + "\');return false;\">Mais informação</a>" +
+        " onclick=\"openPopup(\'" + hotspot.translations[$scope.language].description + "\');return false;\">Mais informação</a>" +
         "</div>";
   };
 
